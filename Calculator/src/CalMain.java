1162bca1786a83cc33c0ff0cdbbb6d98eb2e4454
김별이 <bybit0408@gmail.com>
2021-01-28 16:17:23 +0900

    add design

--- Calculator/src/CalMain.java
+++ Calculator/src/CalMain.java
@@ -1,62 +1,49 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import java.awt.*;
 
-class InsertPanel extends JPanel{//North에 입력칸
-	private JPanel ipanel;
-	public JPanel getIpanel() {
-		return ipanel;
-	}
-	public void setIpanel(JPanel ipanel) {
-		this.ipanel = ipanel;
-	}
-	public InsertPanel() {
-		ipanel.setLayout(new FlowLayout());
-		ipanel.add(new JLabel(" 수식입력"));
-		ipanel.add(new JTextField(20));	//20칸정도 들어갈 크기
-	}
-}
-
-class ButtonPanel extends JPanel {//center에 버튼들
-	//3x4 격자의 GridLayout 배치관리자 생성
-	public ButtonPanel() {
-		setLayout(new GridLayout(4, 4,5,5));
-		JButton[] b = new JButton[16];	
-		for(int i=0;i<10;i++) {//0~9까지의 버튼
-			b[i] = new JButton(Integer.toString(i));
-		}
-		b[10] = new JButton("CE");
-		b[11] = new JButton("계산");
-		b[12] = new JButton("+");
-		b[13] = new JButton("-");
-		b[14] = new JButton("X");
-		b[15] = new JButton("/");
-		for(int i=0;i<b.length;i++) {
-			add(b[i]);
-		}
-	}
-}
-class ResultPanel extends JPanel {//South에 계산 결과 출력
-	public ResultPanel() {
-		setLayout(new GridLayout(1,2, 5, 5));
-		add(new JLabel("계산결과 "));
-		add(new JTextField(""));
-	}
-}
 public class CalMain extends JFrame {
 	public CalMain() {
 		setTitle("계산기 프레임");
+		setSize(450, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//프레임 윈도우 닫으면 프로그램을 종료하도록 설정
 		Container c = getContentPane();
 		
-		//컨텐트팬에 BorderLayout 배치관리자 설정
-		c.setLayout(new BorderLayout());
+		JPanel np = new JPanel();	//north부분
+		JPanel cp = new JPanel(); 	//center부분
+		JPanel sp = new JPanel(); 	//south부분
 		
-		c.add(new InsertPanel().getIpanel(), BorderLayout.NORTH);
-		c.add(new ButtonPanel(), BorderLayout.CENTER);	
-		c.add(new ResultPanel(), BorderLayout.SOUTH);
+		np.setBackground(Color.LIGHT_GRAY);		//배경색
+		cp.setLayout(new GridLayout(4,4,5,5));	//레이아웃 그리드로
+		sp.setBackground(Color.yellow);	
 		
-		setSize(450, 300);
+		c.add(cp);		//위치 배치
+		c.add(np, BorderLayout.NORTH);	
+		c.add(sp, BorderLayout.SOUTH);
+		
+		JLabel is = new JLabel("수식입력");	//(np)
+		JTextField tx = new JTextField(20);	//20칸
+		np.add(is);
+		np.add(tx);
+		
+		for(int i=0;i<16;i++) {	//버튼 생성	(cp)
+			JButton[] b = new JButton[16];
+			String[] str = {"CE", "계산", "+", "-", "X", "/"};
+			if(i < 10) {
+				b[i] = new JButton(Integer.toString(i));
+			}
+			else {
+				b[i] = new JButton(str[i-10]);
+			}
+			cp.add(b[i]);
+		}
+		
+		JLabel rs = new JLabel("계산 결과");	//(sp)
+		JTextField tx2 = new JTextField(20);	
+		sp.add(rs);
+		sp.add(tx2);
+		
 		setVisible(true);
 	}
 
